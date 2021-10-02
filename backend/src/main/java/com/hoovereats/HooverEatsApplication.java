a16bcb3983b7a7c3a8bf217cab693711b5675867
_anhTimeLimitExceeded <anh.t.le22@gmail.com>
2021-10-02 15:32:39 -0400

    Initial Commit

---
+++ backend/src/main/java/com/hoovereats/HooverEatsApplication.java
@@ -0,0 +1,13 @@
+package com.hoovereats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooverEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooverEatsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hoovereats/HooverEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoovereats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooverEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
